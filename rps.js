1a778b995b8cb5127a16485a51f7041fbfcec551
Ryan <rygho90@gmail.com>
2022-04-28 16:25:48 -0400

    Complete 5-round console game functionality

--- rps.js
+++ rps.js
@@ -1,14 +1,74 @@
 function computerPlay() {
     let computerRoll = Math.floor(Math.random() * (3))
-    return CHOICES[computerRoll]
+    return ['ROCK', 'PAPER', 'SCISSORS'][computerRoll]
 }
 
 function playRound() {
-    let playerChoice = prompt("Enter 'Rock', 'Paper', or 'Scissors':")
+    let playerChoice = prompt("Enter 'ROCK', 'PAPER', or 'SCISSORS':").toUpperCase()
     let computerChoice = computerPlay()
-    console.log(`Player chose ${playerChoice}. Computer chose ${computerChoice}.`)
+    let lossWording = `You lost the round. Computer's ${computerChoice} beat your ${playerChoice}.`
+    let winWording = `You win the round! Your ${playerChoice} beat Computer's ${computerChoice}.` 
+
+    if (playerChoice == computerChoice) {
+        console.log(`Tie. You and Computer both chose ${playerChoice}.`)
+        return "T"
+    } 
+    if (playerChoice == "ROCK") {
+        if (computerChoice == "PAPER") {
+            console.log(lossWording)
+            return "L"
+        } else {
+            console.log(winWording)
+            return "W"
+        }
+    }
+    if (playerChoice == "PAPER") {
+        if (computerChoice == "SCISSORS") {
+            console.log(lossWording)
+            return "L"
+        } else {
+            console.log(winWording)
+            return "W"
+        }
+    }
+    if (playerChoice == "SCISSORS") {
+        if (computerChoice == "ROCK") {
+            console.log(lossWording)
+            return "L"
+        } else {
+            console.log(winWording)
+            return "W"
+        }
+    }
 }
 
-const CHOICES = ['Rock', 'Paper', 'Scissors'];
+function game() {
+    playerScore = 0
+    computerScore = 0
+    let roundResult = "";
 
-playRound()
+    for (let i=0; i < 5; i++) {
+        roundResult = playRound()
+        if (roundResult == "W") {
+            playerScore++
+        }
+        if (roundResult == "L") {
+            computerScore++
+        }
+    }
+
+    console.log(`\nPlayer: ${playerScore}. Computer: ${computerScore}.`)
+    if (playerScore == computerScore) {
+        console.log("You tied!")
+    } else if (playerScore > computerScore) {
+        console.log("You won the game!")
+    } else {
+        console.log("You lost the game!")
+    }
+}
+
+let playerScore = 0
+let computerScore = 0
+
+game()
+
