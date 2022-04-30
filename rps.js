15217bc32f249e9e2c62b35e799ddc2b3c3b2a52
Ryan <rygho90@gmail.com>
2022-04-30 01:08:30 -0400

    Complete all basic functionality for improved UI

--- rps.js
+++ rps.js
@@ -5,25 +5,33 @@ function computerPlay() {
 
 function checkWinner() {
     if (playerScore == 5) {
-        playerDisplay.innerText = 'First to 5!'
-        computerDisplay.innerText = 'You win!'
-        playerScore = 0
-        computerScore = 0
+        const gameScreen = document.querySelector('.game')
+        while (gameScreen.firstChild) {
+            gameScreen.removeChild(gameScreen.lastChild);
+        }
+        const playerWin = document.createElement('div');
+        playerWin.innerText = "You Win!";
+        gameScreen.append(playerWin);
     }
     if (computerScore == 5) {
-        playerDisplay.innerText = 'Computer is first to 5.'
-        computerDisplay.innerText = 'You lose!'
-        playerScore = 0
-        computerScore = 0
+        const gameScreen = document.querySelector('.game')
+        while (gameScreen.firstChild) {
+            gameScreen.removeChild(gameScreen.lastChild);
+        }
+        const playerLose = document.createElement('div');
+        playerLose.innerText = "You Lose!";
+        gameScreen.append(playerLose);
     }
 }
-
 
 function playRound(playerChoice) {
     playerChoice = playerChoice.toUpperCase()
     let computerChoice = computerPlay()
     let lossWording = `You lost the round. Computer's ${computerChoice} beat your ${playerChoice}.`
     let winWording = `You win the round! Your ${playerChoice} beat Computer's ${computerChoice}.` 
+
+    playerPick.innerText = `${playerChoice}`;
+    computerPick.innerText = `${computerChoice}`;
 
     if (playerChoice == computerChoice) {
         roundResult.innerText = `Tie. You and Computer both chose ${playerChoice}.`;
@@ -33,12 +41,12 @@ function playRound(playerChoice) {
         if (computerChoice == "PAPER") {
             roundResult.innerText = (lossWording)
             computerScore += 1;
-            computerDisplay.innerText = `Computer: ${computerScore}`;
+            computerDisplay.innerText = `Score: ${computerScore}`;
             checkWinner();
         } else {
             roundResult.innerText = (winWording)
             playerScore += 1;
-            playerDisplay.innerText = `Player: ${playerScore}`;
+            playerDisplay.innerText = `Score: ${playerScore}`;
             checkWinner();
         }
     }
@@ -46,22 +54,26 @@ function playRound(playerChoice) {
         if (computerChoice == "SCISSORS") {
             roundResult.innerText = (lossWording)
             computerScore += 1;
-            computerDisplay.innerText = `Computer: ${computerScore}`;
+            computerDisplay.innerText = `Score: ${computerScore}`;
+            checkWinner();
         } else {
             roundResult.innerText = (winWording)
             playerScore += 1;
-            playerDisplay.innerText = `Player: ${playerScore}`;
+            playerDisplay.innerText = `Score: ${playerScore}`;
+            checkWinner();
         }
     }
     if (playerChoice == "SCISSORS") {
         if (computerChoice == "ROCK") {
             roundResult.innerText = (lossWording)
             computerScore += 1;
-            computerDisplay.innerText = `Computer: ${computerScore}`;
+            computerDisplay.innerText = `Score: ${computerScore}`;
+            checkWinner();
         } else {
             roundResult.innerText = (winWording)
             playerScore += 1;
-            playerDisplay.innerText = `Player: ${playerScore}`;
+            playerDisplay.innerText = `Score: ${playerScore}`;
+            checkWinner();
         }
     }
 }
@@ -69,6 +81,7 @@ function playRound(playerChoice) {
 let playerScore = 0
 let computerScore = 0
 
+/*
 const buttons = document.querySelectorAll('.btn');
 const results = document.querySelector(`.results`);
 const roundResult = document.createElement('div');
@@ -80,6 +93,14 @@ computerDisplay.innerText = `Computer: ${computerScore}`;
 results.append(roundResult);
 results.append(playerDisplay);
 results.append(computerDisplay);
+*/
+
+const buttons = document.querySelectorAll('.btn'); 
+const roundResult = document.querySelector('.round-result');
+const playerDisplay = document.querySelector('.player-display');
+const computerDisplay = document.querySelector('.computer-display');
+const playerPick = document.querySelector('.player-pick');
+const computerPick = document.querySelector('.computer-pick')
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
